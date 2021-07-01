bb1d83b9293b4c88887d9c66062dd6c0682531d4
Reto Gmür <reto@factsmission.com>
2021-07-01 16:20:32 +0000

    added function to read a fixed number of bytes

--- util.ts
+++ util.ts
@@ -16,3 +16,22 @@ export function concat(...elems: Uint8Array[]): Uint8Array {
   }
   return result;
 }
+
+export async function readBytes(reader: Deno.Reader, length: number) {
+  const result = new Uint8Array(length);
+  let bytesRead = 0;
+  while (bytesRead < length) {
+    const remainder = result.subarray(bytesRead);
+    const bytesReadNow = await reader.read(remainder);
+    if (bytesReadNow === null) {
+      throw new Error("End of reader");
+    }
+    bytesRead += bytesReadNow;
+    if (bytesRead !== length) {
+      console.info(
+        `Outstanding data: expecting ${length} bytes but got only ${bytesRead} so far`,
+      );
+    }
+  }
+  return result;
+}
