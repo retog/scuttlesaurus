2e7c0eebb19db53a0f30ab5b0eb92aa9e393cc5e
Reto Gmür <reto@factsmission.com>
2021-07-01 07:45:40 +0000

    passing destination as arg

--- play.ts
+++ play.ts
@@ -6,10 +6,17 @@ const decoder = new TextDecoder();
 const encoder = new TextEncoder();
 const host = new SsbHost();
 
+if (Deno.args.length !== 1) {
+  throw new Error("expecting exactly one argument");
+}
+
+const addressString = Deno.args[0]; // "net:172.17.0.2:8008~shs:bEhA+VRRIf8mTO474KlSuYTObJACRYZqkwxCl4Id4fk="
+const address = parseAddress(
+  addressString,
+);
+
 const boxConnection = await host.connect(
-  parseAddress(
-    "net:172.17.0.2:8008~shs:bEhA+VRRIf8mTO474KlSuYTObJACRYZqkwxCl4Id4fk=",
-  ),
+  address,
 );
 async function monitorConnection() {
   let i = 0;
@@ -33,7 +40,7 @@ console.log("sending a message...");
 boxConnection.sendRpcMessage({
   "name": ["createHistoryStream"],
   "type": "source",
-  "args": [{ "id": "@bEhA+VRRIf8mTO474KlSuYTObJACRYZqkwxCl4Id4fk=.ed25519" }],
+  "args": [{ "id": `@${address.key}.ed25519` }],
 }, {
   bodyType: RpcBodyType.json,
   isStream: true,
