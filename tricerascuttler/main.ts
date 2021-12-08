41ced0d5100c804942efd7d623ef849ccb57c4c9
Reto Gmür <reto@factsmission.com>
2021-12-08 17:15:18 +0100

    proxying query-endpoint

--- tricerascuttler/main.ts
+++ tricerascuttler/main.ts
@@ -1,6 +1,7 @@
 //https://deno.land/x/scuttlesaurus@0.1.0/
 import { Context } from "https://deno.land/x/oak@v10.0.0/context.ts";
 import staticFiles from "https://x.nest.land/static_files@1.1.2/mod.ts";
+import { proxy } from "https://deno.land/x/oak_http_proxy@2.0.0/mod.ts";
 import { createScuttlebuttHost } from "../scuttlesaurus/main.ts";
 import SparqlStorer from "./SparqlStorer.ts";
 import { log } from "../scuttlesaurus/util.ts";
@@ -20,8 +21,6 @@ const sparqlEndpointUpdate = getRequiredEnvVar("SPARQL_ENDPOINT_UPDATE");
 const host = await createScuttlebuttHost();
 const storer = new SparqlStorer(sparqlEndpointQuery, sparqlEndpointUpdate);
 storer.connectAgent(host.feedsAgent!);
-host.controlAppRouter!.get("/query", (ctx: Context) => {
-  ctx.response.body = `TODO: Query proxy to ${sparqlEndpointQuery}`;
-});
+host.controlAppRouter!.all("/query", proxy(sparqlEndpointQuery));
 host.controlApp!.use(staticFiles("static"));
 host.start();
