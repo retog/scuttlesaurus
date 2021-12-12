dd6da94d50a153db99184600f23c4915ef9e8e4c
Reto Gmür <reto@factsmission.com>
2021-12-12 19:46:59 +0100

    header/navigation

---
+++ tricerascuttler/static/HeaderElement.js
@@ -0,0 +1,48 @@
+export class HeaderElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    const template = `
+      <style>
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+      }
+
+      li {
+          float: left;
+      }
+
+      li a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+      }
+
+      li a:hover {
+          background-color: #111;
+      }
+      .active {
+        background-color: #04AA6D;
+      }
+      </style>
+      <ul>
+          <li><a href="/">Explore</a></li>
+          <li><a href="/followees.html">Following</a></li>
+          <li><a href="/peers.html">Peers</a></li>
+          <li><a id="query" href="/query.html">SPARQL</a></li>
+          <li><a id="query" href="/about.html">About</a></li>
+      </ul>
+    `;
+    this.shadowRoot.innerHTML = template;
+    this.shadowRoot.querySelector(`a[href='${document.location.pathname}']`)
+      .setAttribute("class", "active");
+  }
+}
+window.customElements.define("ssb-header", HeaderElement);
