dc39dfc0f2a9cede7736dce6c8092e7d2abd22da
Reto Gmür <reto@factsmission.com>
2021-12-16 00:27:05 +0100

    allowing loading more posts... and more...

--- tricerascuttler/static/js/PostListElement.js
+++ tricerascuttler/static/js/PostListElement.js
@@ -1,12 +1,12 @@
 import * as _PostElement from "./PostElement.js";
 
-async function getPosts(query) {
+async function getPosts(query, offset, limit) {
   const response = await fetch("/query", {
     "headers": {
       "Accept": "application/sparql-results+json,*/*;q=0.9",
       "Content-Type": "application/sparql-query",
     },
-    "body": query + " LIMIT 50",
+    "body": query + `OFFSET ${offset} LIMIT ${limit}`,
     "method": "POST",
   });
   if (response.status >= 300) {
@@ -17,21 +17,25 @@ async function getPosts(query) {
   return resultJson.results.bindings.map((binding) => binding.post.value);
 }
 
+const loadSize = 50;
+
 export class PostListElement extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
 
-    const query = this.getAttribute("query");
+    this.query = this.getAttribute("query");
 
-    let template = `
+    this.currentOffset = 0;
+
+    this.shadowRoot.innerHTML = `
     <style>
-    .contact {
+    .post {
       display: block;
       flex: 0 1 150px;
       margin: 5px;
     }
-    .contacts{
+    .posts{
       display: flex;
       flex-wrap: wrap;
       justify-content: center;
@@ -39,24 +43,47 @@ export class PostListElement extends HTMLElement {
   
     </style>
 
+    <div id="content">
+
+    </div>
+
     `;
-    getPosts(query).then(
+    const contentDiv = this.shadowRoot.getElementById("content");
+    this.getPostsAndAppend(contentDiv);
+  }
+
+  async getPostsAndAppend(targetElement) {
+    await getPosts(this.query, this.currentOffset, loadSize + 1).then(
       (posts) => {
+        this.currentOffset += loadSize;
         if (posts.length > 0) {
-          template += `<h2>${posts.length} posts</h2>
-          <div class="contacts">
+          targetElement.insertAdjacentHTML(
+            "beforeend",
+            `<div class="posts">
           ${
-            [...posts].map((p) =>
-              `<ssb-post src="${p}" class="contact"></ssb-post>`
-            ).join("")
+              [...posts].slice(0, loadSize).map((p) =>
+                `<ssb-post src="${p}" class="post"></ssb-post>`
+              ).join("")
+            }
+          </div>`,
+          );
+          if (posts.length > loadSize) {
+            const showMoreButton = document.createElement("button");
+            showMoreButton.innerHTML = "Show more";
+            targetElement.appendChild(showMoreButton);
+            showMoreButton.addEventListener("click", () => {
+              targetElement.removeChild(showMoreButton);
+              this.getPostsAndAppend(targetElement);
+            });
           }
-          </div>`;
         } else {
-          template += `No posts found with given query: <code><pre>${
-            query.replaceAll("<", "&lt;")
-          }</pre></code>`;
+          targetElement.insertAdjacentHTML(
+            "beforeend",
+            `No posts found with given query: <code><pre>${
+              query.replaceAll("<", "&lt;")
+            }</pre></code>`,
+          );
         }
-        this.shadowRoot.innerHTML = template;
       },
     );
   }
