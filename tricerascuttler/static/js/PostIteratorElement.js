b3b4b33d2b435d592bda0cc2dc7f95d194227b3f
Reto Gmür <reto@factsmission.com>
2022-01-06 22:33:51 +0100

    iterative version of read-page
    turns out to be much slower. Keeping for now to try with other triple stores.

---
+++ tricerascuttler/static/js/PostIteratorElement.js
@@ -0,0 +1,39 @@
+import * as _PostElement from "./PostElement.js";
+import { PostListElement } from "./PostListElement.js";
+import { runQuery } from "./web-util.js";
+
+export class PostIteratorElement extends PostListElement {
+  posts = [];
+
+  constructor() {
+    super();
+    this.anchor = this.getAttribute("anchor");
+    this.step = this.getAttribute("step");
+  }
+
+  async nextPost() {
+    const effQuery = this.step.replaceAll("??", `<${this.anchor}>`);
+    const resultBindings = (await runQuery(effQuery)).results.bindings;
+    if (resultBindings.length === 0) {
+      //no more posts
+      return;
+    } else {
+      const post = resultBindings[0].post.value;
+      this.anchor = post;
+      const show = JSON.parse(resultBindings[0].show.value);
+      if (show) {
+        return post;
+      } else {
+        return this.nextPost();
+      }
+    }
+  }
+
+  async getPosts(offset, limit) {
+    for (let i = 0; i < limit; i++) {
+      this.posts[i] = await this.nextPost();
+    }
+    return this.posts;
+  }
+}
+window.customElements.define("ssb-post-iter", PostIteratorElement);
