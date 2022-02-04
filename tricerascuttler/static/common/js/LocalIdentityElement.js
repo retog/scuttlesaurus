14707b04d181cefaeb8e1467a641e16c40c057e3
Reto Gmür <reto@factsmission.com>
2022-02-04 13:09:31 +0000

    allow downloading identity secret

--- tricerascuttler/static/common/js/LocalIdentityElement.js
+++ tricerascuttler/static/common/js/LocalIdentityElement.js
@@ -1,4 +1,4 @@
-import { FeedId } from "./ext/scuttlebutt-host.js";
+import { FeedId, toBase64 } from "./ext/scuttlebutt-host.js";
 export class LocalIdentityElement extends HTMLElement {
   constructor() {
     super();
@@ -6,6 +6,12 @@ export class LocalIdentityElement extends HTMLElement {
   }
 
   async connectedCallback() {
+    const certBlob = new Blob([
+      serializeKeyPair(
+        (await window.scuttlebuttHost).getClientKeyPair(),
+      ),
+    ], { type: "application/json" });
+    const certURL = URL.createObjectURL(certBlob);
     this.shadowRoot.innerHTML = `
     <main id="main">
     <h1>Your identity</h1>
@@ -15,7 +21,7 @@ export class LocalIdentityElement extends HTMLElement {
         .toString()
     }
     <p>
-        You can download the identity an use it with another Scuttlebutt client or upload an identity
+        You can download your identity secret an use it with another Scuttlebutt client or upload an identity
         created
         elsewhere, however you must no use the same identity with multiple clients as this would fork
         and thus
@@ -23,8 +29,26 @@ export class LocalIdentityElement extends HTMLElement {
         After transfering your identity give the new client enough time to get your latest messages
         before creating
         new messages.</p>
+        <div id="links"></div>
     </main>
     `;
+
+    const links = this.shadowRoot.getElementById("links");
+    const link = document.createElement("a");
+    link.download = "secret.json";
+    link.href = certURL;
+    link.innerText = "Download Identity Secret";
+
+    links.appendChild(link);
   }
 }
 window.customElements.define("ssb-local-identity", LocalIdentityElement);
+
+function serializeKeyPair(keyPair) {
+  const secret = {
+    public: toBase64(keyPair.publicKey) + ".ed25519",
+    "private": toBase64(keyPair.privateKey) + ".ed25519",
+    curve: keyPair.keyType,
+  };
+  return JSON.stringify(secret, undefined, 2);
+}
