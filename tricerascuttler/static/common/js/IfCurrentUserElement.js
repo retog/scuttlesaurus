33c85b8f693eed4a29e8d10c79ebf03df71f2dec
Reto Gmür <reto@factsmission.com>
2022-02-08 18:41:21 +0000

    element to conditionally show content

---
+++ tricerascuttler/static/common/js/IfCurrentUserElement.js
@@ -0,0 +1,37 @@
+import {
+  FeedId,
+  parseAddress,
+  parseKeyPair,
+  serializeKeyPair,
+} from "./ext/scuttlebutt-host.js";
+import { sigilToIri } from "./web-util.js";
+import * as _instanceName from "./InstanceNameElement.js";
+import * as _feedAuthor from "./FeedAuthorLinkElement.js";
+import * as _feedAuthorEditor from "./FeedAuthorEditorElement.js";
+export class IfCurrentUserElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  async connectedCallback() {
+    const feedUri = this.getAttribute("feed");
+    const scuttlebuttHost = await window.scuttlebuttHost;
+    const localId = sigilToIri(scuttlebuttHost.identity.toString());
+    if (feedUri === localId.toString()) {
+      if (
+        this.children.length > 0 && this.children[0].tagName === "TEMPLATE"
+      ) {
+        this.shadowRoot.replaceChildren(
+          this.children[0].content.cloneNode(true),
+        );
+      } else {
+        this.shadowRoot.replaceChildren(...this.children);
+      }
+    } else {
+      this.shadowRoot.innerHTML = "Naaa";
+      console.log("naaa", feedUri, localId.toString());
+    }
+  }
+}
+window.customElements.define("ssb-if-current-user", IfCurrentUserElement);
