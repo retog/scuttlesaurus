76f25cc440922bf9a25d6ae1c8cf6ebd89400fcb
Reto Gmür <reto@factsmission.com>
2022-02-15 14:41:25 +0000

    Added follow/unfollow
    and using async/await

--- tricerascuttler/static/common/js/FeedAuthorElement.js
+++ tricerascuttler/static/common/js/FeedAuthorElement.js
@@ -1,4 +1,10 @@
-import { handleSsbLinks, iriToSigil, mdToHtml, runQuery } from "./web-util.js";
+import {
+  handleSsbLinks,
+  iriToSigil,
+  mdToHtml,
+  runQuery,
+  sigilToIri,
+} from "./web-util.js";
 
 const syncIcon = `
   <?xml version="1.0" encoding="iso-8859-1"?>
@@ -87,18 +93,20 @@ export class FeedAuthorElement extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    const feedUri = this.getAttribute("src");
-    const mode = this.getAttribute("mode");
-    getDescription(feedUri).then(
-      ({ name, description, image }) => {
-        const renderedDescription = description
-          ? mode === "small"
-            ? description.substring(0, 80)
-            : mdToHtml(description) +
-              "<br/>"
-          : "";
+    this.feedUri = this.getAttribute("src");
+  }
 
-        const template = `
+  async connectedCallback() {
+    const mode = this.getAttribute("mode");
+    const { name, description, image } = await getDescription(this.feedUri);
+    const renderedDescription = description
+      ? mode === "small"
+        ? description.substring(0, 80)
+        : mdToHtml(description) +
+          "<br/>"
+      : "";
+
+    const template = `
       <style>
         :host {
           background-color: lavenderblush;
@@ -110,65 +118,89 @@ export class FeedAuthorElement extends HTMLElement {
           padding: 5pt;
         }
         ${
-          mode === "small"
-            ? `img {
+      mode === "small"
+        ? `img {
             max-width: 40px;
             max-height: 40px;
           }`
-            : `img {
+        : `img {
               max-width: 100%;
             }`
-        }
+    }
       </style>
     
       <div>
-      <a href="/?uri=${feedUri}">${
-          name ? name : feedUri.substring(0, 23) + "..."
-        }</a>
+      <a href="/?uri=${this.feedUri}">${
+      name ? name : this.feedUri.substring(0, 23) + "..."
+    }</a>
       <svg id="sync" width="20pt" height="20pt">${syncIcon}</svg><br/>
       ${renderedDescription}
       ${image ? `<img src="${image.replace("ssb:blob/", "./blob/")}">` : ""}
       </div>
       <slot></slot>
     `;
-        this.shadowRoot.innerHTML = template;
-        const syncButton = this.shadowRoot.getElementById("sync");
-        syncButton.addEventListener("click", async () => {
-          await fetch("./followees", {
-            "headers": {
-              "Accept": "application/json,*/*;q=0.9",
-              "Content-Type": "application/json",
-            },
-            "body": JSON.stringify({ "id": iriToSigil(feedUri) }),
-            "method": "POST",
-          });
-          syncButton.parentNode.removeChild(syncButton);
-        });
-        handleSsbLinks(this.shadowRoot);
-        const copyUriButton = document.createElement("button");
-        copyUriButton.innerHTML = "Copy URI";
-        copyUriButton.onclick = async () => {
-          try {
-            await navigator.clipboard.writeText(feedUri);
-            alert("Message URI copied to clipboard");
-          } catch (_error) {
-            console.log(`Failed writing to clipboard: ${feedUri}`);
-          }
-        };
-        this.shadowRoot.append(copyUriButton);
-        const copySigilButton = document.createElement("button");
-        copySigilButton.innerHTML = "Copy Sigil";
-        copySigilButton.onclick = async () => {
-          try {
-            await navigator.clipboard.writeText(iriToSigil(feedUri));
-            alert("Message Sigil copied to clipboard");
-          } catch (_error) {
-            console.log(`Failed writing to clipboard: ${iriToSigil(feedUri)}`);
-          }
-        };
-        this.shadowRoot.append(copySigilButton);
-      },
-    );
+    this.shadowRoot.innerHTML = template;
+    const syncButton = this.shadowRoot.getElementById("sync");
+    syncButton.addEventListener("click", async () => {
+      await fetch("./followees", {
+        "headers": {
+          "Accept": "application/json,*/*;q=0.9",
+          "Content-Type": "application/json",
+        },
+        "body": JSON.stringify({ "id": iriToSigil(this.feedUri) }),
+        "method": "POST",
+      });
+      syncButton.parentNode.removeChild(syncButton);
+    });
+    handleSsbLinks(this.shadowRoot);
+    this.shadowRoot.append(await this.createFollowUnFollowButton());
+    const copyUriButton = document.createElement("button");
+    copyUriButton.innerHTML = "Copy URI";
+    copyUriButton.onclick = async () => {
+      try {
+        await navigator.clipboard.writeText(this.feedUri);
+        alert("Message URI copied to clipboard");
+      } catch (_error) {
+        console.log(`Failed writing to clipboard: ${this.feedUri}`);
+      }
+    };
+    this.shadowRoot.append(copyUriButton);
+    const copySigilButton = document.createElement("button");
+    copySigilButton.innerHTML = "Copy Sigil";
+    copySigilButton.onclick = async () => {
+      try {
+        await navigator.clipboard.writeText(iriToSigil(this.feedUri));
+        alert("Message Sigil copied to clipboard");
+      } catch (_error) {
+        console.log(`Failed writing to clipboard: ${iriToSigil(this.feedUri)}`);
+      }
+    };
+    this.shadowRoot.append(copySigilButton);
+  }
+
+  async createFollowUnFollowButton() {
+    const scuttlebuttHost = await window.scuttlebuttHost;
+    const localId = sigilToIri(scuttlebuttHost.identity.toString());
+    if (this.feedUri === localId.toString()) {
+      return document.createElement("span");
+    }
+    let isFollowing = (await runQuery(`ASK { 
+      <${localId}> <ssb:ontology:derivatives:follows> <${this.feedUri}>. 
+    } `)).boolean;
+    const button = document.createElement("button");
+    const setLabel = () =>
+      button.innerHTML = isFollowing ? "Unfollow" : "Follow";
+    setLabel();
+    button.onclick = async () => {
+      await scuttlebuttHost.publish({
+        type: "contact",
+        contact: iriToSigil(this.feedUri),
+        following: !isFollowing,
+      });
+      isFollowing = !isFollowing;
+      setLabel();
+    };
+    return button;
   }
 }
 window.customElements.define("ssb-feed-author", FeedAuthorElement);
