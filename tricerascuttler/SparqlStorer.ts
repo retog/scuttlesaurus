7c21c393603438c502a499cdc7052f39b8dc011d
Reto Gmür <reto@factsmission.com>
2022-02-16 18:38:21 +0000

    dealing with case that first messages are missing

--- tricerascuttler/SparqlStorer.ts
+++ tricerascuttler/SparqlStorer.ts
@@ -95,9 +95,13 @@ error: Uncaught (in promise) TypeError: error sending request for url (http://fu
     PREFIX ssb: <ssb:ontology:>
     
     SELECT ?next WHERE {
-      ?msg rdf:type ssb:Message;
+      {
+        ?msg rdf:type ssb:Message;
            ssb:author <${feedId.toUri()}>;
           ssb:seq ?seq.
+      } UNION {
+        BIND (0 AS ?seq)
+      }
       BIND((?seq+1) AS ?next)
       MINUS  {
         ?otherMsg rdf:type ssb:Message;
