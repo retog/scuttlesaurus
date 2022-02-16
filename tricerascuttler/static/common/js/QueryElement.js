a4d7b98f015671028c049ffddce6682fa4c19460
Reto Gmür <reto@factsmission.com>
2022-02-16 16:59:58 +0000

    added examples , fixes dislay issues

--- tricerascuttler/static/common/js/QueryElement.js
+++ tricerascuttler/static/common/js/QueryElement.js
@@ -1,10 +1,38 @@
 import * as _Tabs from "https://unpkg.com/@triply/yasgui/build/yasgui.min.js";
+
+const examples = [
+  {
+    name: "List feeds",
+    value: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ssb: <ssb:ontology:>
+    PREFIX ssbx: <ssb:ontology:derivatives:>
+    SELECT DISTINCT ?author (count(?msg) as ?msgCount) (max(?seq) as ?maxSeq) { 
+      ?msg ssb:author ?author;
+           ssb:seq ?seq. 
+    } LIMIT 10`,
+  },
+];
+
 export class QueryElement extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
 
     this.shadowRoot.innerHTML = `
+    <style>
+      .yasgui .autocompleteWrapper {
+        display: none !important;
+      }
+      .yasgui .tabContextButton {
+        display: none !important;
+      }
+      .yasgui .yasqe_share {
+        display: none !important;
+      }
+      #yasgui {
+
+      }
+    </style>
     <link href="https://unpkg.com/@triply/yasgui/build/yasgui.min.css" rel="stylesheet" type="text/css" />
     <div id="yasgui"></div>`;
     const yasgui = new Yasgui(this.shadowRoot.getElementById("yasgui"), {
@@ -13,7 +41,20 @@ export class QueryElement extends HTMLElement {
         method: "GET",
       },
       copyEndpointOnNewTab: false,
+      populateFromUrl: false,
+      autofocus: true,
     });
+    if (Object.keys(yasgui._tabs).length === 1) {
+      const firstTab = yasgui.getTab();
+      examples.forEach((example) => {
+        const tab = yasgui.addTab(
+          true, // set as active tab
+          { ...Yasgui.Tab.getDefaults(), name: example.name },
+        );
+        tab.setQuery(example.value);
+      });
+      yasgui.markTabSelected(firstTab.getId());
+    }
   }
 }
 window.customElements.define("ssb-query", QueryElement);
