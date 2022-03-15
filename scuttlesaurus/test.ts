cb68ed255983011500046cd83c94ae76e4fc208c
Reto Gmür <reto@factsmission.com>
2022-03-15 09:03:29 +0000

    Added Test with WS

--- scuttlesaurus/test.ts
+++ scuttlesaurus/test.ts
@@ -50,3 +50,52 @@ Deno.test("Server -> Client Message Flow", async () => {
   await serverP;
   await clientP;
 });
+
+Deno.test("Server -> Client Message Flow via WS", async () => {
+  const controller = new AbortController();
+  const serverDir = await Deno.makeTempDir({
+    prefix: "scuttlesuarus-test-server",
+  });
+  const port: number = await getFreePort(9090);
+  const server = new DenoScuttlebuttHost({
+    baseDir: serverDir,
+    transport: {
+      ws: {
+        web: ["access"],
+      },
+    },
+    web: {
+      access: {
+        port,
+      },
+    },
+  });
+  const serverP = server.start(controller.signal);
+  const serverAddress = `ws:127.0.0.1:${port}~shs:${server.identity.base64Key}`;
+  const clientDir = await Deno.makeTempDir({
+    prefix: "scuttlesuarus-test-client",
+  });
+  const client = new DenoScuttlebuttHost({
+    baseDir: clientDir,
+    peers: [serverAddress],
+    follow: [server.identity.toString()],
+    transport: {
+      ws: {
+        web: [],
+      },
+    },
+  });
+  const clientP = client.start(controller.signal);
+  server.publish({
+    type: "test",
+    value: 543,
+  });
+  await delay(500);
+  const feed = client.feedsAgent?.getFeed(server.identity);
+  const firstMsg = (await feed?.[Symbol.asyncIterator]().next())!.value;
+  assertEquals(firstMsg.value.content.value, 543);
+  controller.abort();
+  await delay(600); //allows rpc-timeout checker to realize connection is closed
+  await serverP;
+  await clientP;
+});
