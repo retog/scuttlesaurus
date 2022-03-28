fbbd679d5715c901e6e77ae91c1ceaf9361c3888
Reto Gmür <reto@factsmission.com>
2022-03-28 22:17:35 +0000

    Storing ranking table as binary

--- scuttlesaurus/storage/fs/FsStorage.ts
+++ scuttlesaurus/storage/fs/FsStorage.ts
@@ -21,22 +21,41 @@ export default class FsStorage
   implements BlobsStorage, FeedsStorage, RankingTableStorage {
   rankingTableFile;
   constructor(public readonly dataDir: string) {
-    this.rankingTableFile = path.join(dataDir, "ranking-table.json");
+    this.rankingTableFile = path.join(dataDir, "ranking-table.bin");
     Deno.mkdirSync(dataDir, { recursive: true });
   }
 
   async storeFeedPeerRankings(table: Uint8Array[]): Promise<void> {
-    const fileContent = JSON.stringify(
-      table.map((u) => toHex(u)),
-      undefined,
-      2,
-    );
-    await Deno.writeTextFile(this.rankingTableFile, fileContent);
+    const asByteArray = () => {
+      if ((table.length === 0) || (table[0].length === 0)) {
+        return new Uint8Array(0);
+      }
+      const rowLength = table[0].length;
+      //the first 32b contain the row length
+      const buffer = new ArrayBuffer(4 + table.length * rowLength);
+      const rowLengthView = new DataView(buffer, 0, 4);
+      rowLengthView.setInt32(0, rowLength);
+      const bytes = new Uint8Array(buffer, 4);
+      for (let i = 0; i < table.length; i++) {
+        bytes.set(table[i], i * rowLength);
+      }
+      return new Uint8Array(buffer);
+    };
+    await Deno.writeFile(this.rankingTableFile, asByteArray());
   }
 
   async getFeedPeerRankings(): Promise<Uint8Array[]> {
-    const fileContent = await Deno.readTextFile(this.rankingTableFile);
-    return JSON.parse(fileContent);
+    const { buffer } = await Deno.readFile(this.rankingTableFile);
+    const rowLengthView = new DataView(buffer, 0, 4);
+    const rowLength = rowLengthView.getInt32(0);
+    const bytes = new Uint8Array(buffer, 4);
+    //why do we get excess data?
+    const excessBytes = bytes.length % rowLength
+    const table = new Array((bytes.length - excessBytes) / rowLength);
+    for (let i = 0; i < table.length; i++) {
+      table[i] = bytes.subarray(i * rowLength, (i+1) * rowLength);
+    }
+    return table;
   }
 
   private getFeedDir(feedKey: FeedId) {
